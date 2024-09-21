719b7e94e5340573346da23099a2e0fdc2d3e2a5
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-09-22 00:03:15 +0800

    add the editor finder

---
+++ cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/finder/IEditorFinder.kt
@@ -0,0 +1,17 @@
+package io.github.gdrfgdrf.cutebedwars.abstracts.finder
+
+import io.github.gdrfgdrf.cutebedwars.abstracts.game.editing.AbstractEditor
+import io.github.gdrfgdrf.multimodulemediator.Mediator
+import io.github.gdrfgdrf.multimodulemediator.annotation.KotlinSingleton
+import io.github.gdrfgdrf.multimodulemediator.annotation.Service
+import org.bukkit.command.CommandSender
+
+@Service("editor_finder")
+@KotlinSingleton
+interface IEditorFinder {
+    fun find(sender: CommandSender, onFound: (AbstractEditor<*>) -> Unit): IFindResult
+
+    companion object {
+        fun get(): IEditorFinder = Mediator.get(IEditorFinder::class.java)!!
+    }
+}
---
+++ cute-bedwars-finder/src/main/java/io/github/gdrfgdrf/cutebedwars/finder/EditorFinder.kt
@@ -0,0 +1,34 @@
+package io.github.gdrfgdrf.cutebedwars.finder
+
+import io.github.gdrfgdrf.cutebedwars.abstracts.finder.IEditorFinder
+import io.github.gdrfgdrf.cutebedwars.abstracts.finder.IFindResult
+import io.github.gdrfgdrf.cutebedwars.abstracts.game.editing.AbstractEditor
+import io.github.gdrfgdrf.cutebedwars.abstracts.game.editing.IEditors
+import io.github.gdrfgdrf.cutebedwars.finder.result.FindResult
+import io.github.gdrfgdrf.cutebedwars.languages.collect.EditorLanguage
+import io.github.gdrfgdrf.cutebedwars.locale.localizationScope
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+object EditorFinder : IEditorFinder {
+    override fun find(sender: CommandSender, onFound: (AbstractEditor<*>) -> Unit): IFindResult {
+        val findResult = FindResult()
+        val uuid = if (sender is Player) {
+            sender.uniqueId.toString()
+        } else {
+            "not_a_player"
+        }
+
+        val editor = IEditors.get().get(uuid)
+        if (editor == null) {
+            localizationScope(sender) {
+                message(EditorLanguage.NOT_IN_EDITING_MODE)
+                    .send()
+            }
+        } else {
+            onFound(editor)
+        }
+
+        return findResult
+    }
+}
