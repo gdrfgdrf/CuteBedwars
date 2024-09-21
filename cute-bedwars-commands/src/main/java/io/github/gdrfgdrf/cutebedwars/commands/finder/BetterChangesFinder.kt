3dfe98cb239d668178bc243f79516aedff7324cf
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-09-22 00:26:41 +0800

    better changes finder

---
+++ cute-bedwars-commands/src/main/java/io/github/gdrfgdrf/cutebedwars/commands/finder/BetterChangesFinder.kt
@@ -0,0 +1,17 @@
+package io.github.gdrfgdrf.cutebedwars.commands.finder
+
+import io.github.gdrfgdrf.cutebedwars.abstracts.finder.IChangesFinder
+import io.github.gdrfgdrf.cutebedwars.abstracts.game.editing.IChanges
+import org.bukkit.command.CommandSender
+
+object BetterChangesFinder {
+    fun find(sender: CommandSender): IChanges<*>? {
+        var changes: IChanges<*>? = null
+
+        IChangesFinder.get().find(sender) {
+            changes = it
+        }
+
+        return changes
+    }
+}
