7bb2daea55b53ae0790cdc4e44d8af8a0d888a84
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-11-17 14:21:24 +0800

    present stopped from the stop signal constructor

--- cute-bedwars-utils/src/main/java/io/github/gdrfgdrf/cutebedwars/utils/StopSignal.kt
+++ cute-bedwars-utils/src/main/java/io/github/gdrfgdrf/cutebedwars/utils/StopSignal.kt
@@ -4,9 +4,8 @@ import io.github.gdrfgdrf.cutebedwars.abstracts.utils.IStopSignal
 import io.github.gdrfgdrf.multimodulemediator.annotation.ServiceImpl
 
 @ServiceImpl("stop_signal")
-class StopSignal(
-    override var stopped: Boolean
-) : IStopSignal {
+class StopSignal: IStopSignal {
+    override var stopped: Boolean = false
 
     override fun stop() {
         stopped = true
