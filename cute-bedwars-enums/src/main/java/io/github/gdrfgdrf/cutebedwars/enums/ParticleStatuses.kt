fe90f5a405e1c19359d87ea4f027637837b0d658
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-11-17 14:03:37 +0800

    change the parameter of enum service of ParticleStatuses from particle_statues to particle_statuses

--- cute-bedwars-enums/src/main/java/io/github/gdrfgdrf/cutebedwars/enums/ParticleStatuses.kt
+++ cute-bedwars-enums/src/main/java/io/github/gdrfgdrf/cutebedwars/enums/ParticleStatuses.kt
@@ -3,7 +3,7 @@ package io.github.gdrfgdrf.cutebedwars.enums
 import io.github.gdrfgdrf.cutebedwars.abstracts.enums.IParticleStatuses
 import io.github.gdrfgdrf.multimodulemediator.annotation.EnumServiceImpl
 
-@EnumServiceImpl("particle_statues")
+@EnumServiceImpl("particle_statuses")
 enum class ParticleStatuses : IParticleStatuses {
     DEFAULT,
     ACTIVATED,
