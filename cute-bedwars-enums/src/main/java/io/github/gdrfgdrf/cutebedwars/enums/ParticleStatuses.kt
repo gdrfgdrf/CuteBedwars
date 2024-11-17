f2f968745e60842ed58b8932c9d13dc22912a997
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-11-17 13:59:21 +0800

    change enum service to enum service impl in particle statuses

--- cute-bedwars-enums/src/main/java/io/github/gdrfgdrf/cutebedwars/enums/ParticleStatuses.kt
+++ cute-bedwars-enums/src/main/java/io/github/gdrfgdrf/cutebedwars/enums/ParticleStatuses.kt
@@ -1,9 +1,9 @@
 package io.github.gdrfgdrf.cutebedwars.enums
 
 import io.github.gdrfgdrf.cutebedwars.abstracts.enums.IParticleStatuses
-import io.github.gdrfgdrf.multimodulemediator.annotation.EnumService
+import io.github.gdrfgdrf.multimodulemediator.annotation.EnumServiceImpl
 
-@EnumService("particle_statues")
+@EnumServiceImpl("particle_statues")
 enum class ParticleStatuses : IParticleStatuses {
     DEFAULT,
     ACTIVATED,
