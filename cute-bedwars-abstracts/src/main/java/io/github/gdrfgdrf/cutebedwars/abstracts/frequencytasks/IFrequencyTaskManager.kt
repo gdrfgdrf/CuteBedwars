42c87495f1c9f790b23294f27ad6f30b02576d14
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-11-17 14:54:47 +0800

    add the instance getter for frequency task manager

--- cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/frequencytasks/IFrequencyTaskManager.kt
+++ cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/frequencytasks/IFrequencyTaskManager.kt
@@ -1,6 +1,7 @@
 package io.github.gdrfgdrf.cutebedwars.abstracts.frequencytasks
 
 import io.github.gdrfgdrf.cutebedwars.abstracts.utils.IStopSignal
+import io.github.gdrfgdrf.multimodulemediator.Mediator
 import io.github.gdrfgdrf.multimodulemediator.annotation.KotlinSingleton
 import io.github.gdrfgdrf.multimodulemediator.annotation.Service
 
@@ -12,4 +13,8 @@ interface IFrequencyTaskManager {
 
     fun add(frequencyTask: IFrequencyTask): IStopSignal
     fun remove(frequencyTask: IFrequencyTask)
+
+    companion object {
+        fun instance(): IFrequencyTaskManager = Mediator.get(IFrequencyTaskManager::class.java)!!
+    }
 }
