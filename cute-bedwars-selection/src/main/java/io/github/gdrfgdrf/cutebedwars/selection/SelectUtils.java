d3e21cb7d948280b984b253d984185f8e3ada437
Sywyar <sywyari@163.com>
2024-11-21 00:49:26 +0800

    Correctly displayed the selection area

---
+++ cute-bedwars-selection/src/main/java/io/github/gdrfgdrf/cutebedwars/selection/SelectUtils.java
@@ -0,0 +1,24 @@
+package io.github.gdrfgdrf.cutebedwars.selection;
+
+import io.github.gdrfgdrf.cutebedwars.beans.pojo.common.Coordinate;
+
+public class SelectUtils {
+    private final Coordinate centerPoint;
+
+    public SelectUtils(Coordinate centerPoint) {
+        this.centerPoint = centerPoint;
+    }
+
+    public Coordinate fixPoint(Coordinate point) {
+        return fixPoint(centerPoint, point);
+    }
+
+    public static Coordinate fixPoint(Coordinate centerPoint, Coordinate point) {
+        Coordinate coordinate = new Coordinate();
+        coordinate.setX(point.getX() > centerPoint.getX() ? point.getX() + 1 : point.getX());
+        coordinate.setY(point.getY() > centerPoint.getY() ? point.getY() + 1 : point.getY());
+        coordinate.setZ(point.getZ() > centerPoint.getZ() ? point.getZ() + 1 : point.getZ());
+        return coordinate;
+    }
+
+}
