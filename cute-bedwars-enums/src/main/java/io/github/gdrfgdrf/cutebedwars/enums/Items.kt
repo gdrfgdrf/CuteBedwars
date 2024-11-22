fd3594a60c99b09dde1b57706b6a7b7c023dd906
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-11-22 18:33:53 +0800

    remove dev tool item

--- cute-bedwars-enums/src/main/java/io/github/gdrfgdrf/cutebedwars/enums/Items.kt
+++ cute-bedwars-enums/src/main/java/io/github/gdrfgdrf/cutebedwars/enums/Items.kt
@@ -75,53 +75,6 @@ enum class Items(private val item: IItem) : IItems {
           }
       }.build(true)
     ),
-    DEV_TOOL(
-        IItemBuilder.new().modify {
-            material = Material.DIAMOND_PICKAXE
-            name = {
-                LanguageString("dev_tool")
-            }
-            lores.add("test lore 1", "test lore 2", "test lore 3")
-            unbreakable = true
-            movable = false
-            canDestroy.add(Material.STONE)
-            onClick = {
-                val player = it.player
-                player.sendMessage("onClick")
-            }
-            onLeftClick = {
-                val player = it.player
-                player.sendMessage("onLeftClick")
-            }
-            onRightClick = {
-                val player = it.player
-                player.sendMessage("onRightClick")
-            }
-        }.build(true)
-    ),
-    DEV_TOOL_2(
-        IItemBuilder.new().modify {
-            material = Material.DIAMOND_BLOCK
-            name = {
-                LanguageString("dev_tool_2")
-            }
-            lores.add("test lore 1 (2)", "test lore 2 (2)", "test lore 3 (2)")
-            canPlaceOn.add(Material.STONE)
-            onClick = {
-                val player = it.player
-                player.sendMessage("onClick (2)")
-            }
-            onLeftClick = {
-                val player = it.player
-                player.sendMessage("onLeftClick (2)")
-            }
-            onRightClick = {
-                val player = it.player
-                player.sendMessage("onRightClick (2)")
-            }
-        }.build(true)
-    )
-
 
     ;
 
