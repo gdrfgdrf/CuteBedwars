e876916b34156fbc8461de363804ada29a436937
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-11-23 12:30:34 +0800

    change frequency 100 to 1000 in SELECTION_TOOL to fix the lagging

--- cute-bedwars-enums/src/main/java/io/github/gdrfgdrf/cutebedwars/enums/Items.kt
+++ cute-bedwars-enums/src/main/java/io/github/gdrfgdrf/cutebedwars/enums/Items.kt
@@ -81,7 +81,7 @@ enum class Items(private val item: IItem) : IItems {
                   select.pos1(coordinate)
                   event.isCancelled = true
                   asyncTask {
-                      select.trySpawnParticle(Particle.END_ROD, 100)
+                      select.trySpawnParticle(Particle.END_ROD, 1000)
                   }
 
                   updateLores(select, commonItem)
@@ -113,7 +113,7 @@ enum class Items(private val item: IItem) : IItems {
                   select.pos2(coordinate)
                   event.isCancelled = true
                   asyncTask {
-                      select.trySpawnParticle(Particle.END_ROD, 100)
+                      select.trySpawnParticle(Particle.END_ROD, 1000)
                   }
 
                   updateLores(select, commonItem)
