c3a0c8c6e6f42f92d41e7c27676ed86660cd33aa
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-11-23 20:29:38 +0800

    dev circle

--- cute-bedwars-commands/src/main/java/io/github/gdrfgdrf/cutebedwars/commands/sub/Dev.kt
+++ cute-bedwars-commands/src/main/java/io/github/gdrfgdrf/cutebedwars/commands/sub/Dev.kt
@@ -5,6 +5,10 @@ import io.github.gdrfgdrf.cutebedwars.abstracts.commands.IParamCombination
 import io.github.gdrfgdrf.cutebedwars.abstracts.enums.ICommands
 import io.github.gdrfgdrf.cutebedwars.abstracts.enums.IItems
 import io.github.gdrfgdrf.cutebedwars.abstracts.items.IItem
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.base.IPoint2D
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.base.IPoint3D
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.common.ICircle2D
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.mathNumber
 import io.github.gdrfgdrf.cutebedwars.abstracts.particles.IParticles
 import io.github.gdrfgdrf.cutebedwars.abstracts.selection.ISelection
 import io.github.gdrfgdrf.cutebedwars.abstracts.selection.ISelections
@@ -23,8 +27,32 @@ object Dev : AbstractSubCommand(
     override fun description(): LanguageString? = null
 
     override fun run(sender: CommandSender, args: Array<String>, paramCombination: IParamCombination) {
-        ISelections.instance().create(sender as Player)
-        val selectionTool = IItems.valueOf("SELECTION_TOOL")
-        selectionTool.item().give(sender)
+        if (args[0] == "0") {
+            ISelections.instance().create(sender as Player)
+            val selectionTool = IItems.valueOf("SELECTION_TOOL")
+            selectionTool.item().give(sender)
+            return
+        }
+        if (args[0] == "1") {
+            val centerX = args[1].toDoubleOrNull() ?: return
+            val centerY = args[2].toDoubleOrNull() ?: return
+            val R = args[3].toDoubleOrNull() ?: return
+            val step = args[4].toDoubleOrNull() ?: return
+
+            val center = IPoint2D.new(centerX, centerY)
+            val circle2d = ICircle2D.new(center, R.mathNumber())
+
+            runCatching {
+                val divideResult = circle2d.divide(step.mathNumber())
+                if (divideResult.isEmpty()) {
+                    sender.sendMessage("empty")
+                }
+                divideResult.forEach {
+                    sender.sendMessage(it.toString())
+                }
+            }.onFailure {
+                it.printStackTrace()
+            }
+        }
     }
 }
