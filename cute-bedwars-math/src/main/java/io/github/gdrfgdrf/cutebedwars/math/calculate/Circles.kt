19789e44f3062fc3a53631b4735d059689e4040f
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-11-24 01:41:09 +0800

    abstract circles

--- cute-bedwars-math/src/main/java/io/github/gdrfgdrf/cutebedwars/math/calculate/Circles.kt
+++ cute-bedwars-math/src/main/java/io/github/gdrfgdrf/cutebedwars/math/calculate/Circles.kt
@@ -2,22 +2,19 @@ package io.github.gdrfgdrf.cutebedwars.math.calculate
 
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.IMathNumber
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.base.IPoint2D
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.calculate.ICircles
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.mathNumber
 import io.github.gdrfgdrf.cutebedwars.math.common.Point2D
+import io.github.gdrfgdrf.multimodulemediator.annotation.ServiceImpl
 
-object Circles {
-
-    /**
-     * 根据角度获取圆上一点，设圆心为 x0, y0，半径为 R，角度为 a，若要进行旋转则需要 offset 参数，方向为顺时针
-     * 圆上一点 x' = x0 + R * cos(a)
-     * 圆上一点 y' = y0 + R * sin(a)
-     */
-    fun getAPointOnTheCircleFromTheAngle(
+@ServiceImpl("circles")
+object Circles : ICircles {
+    override fun getAPointOnTheCircleFromTheAngle(
         x0: IMathNumber,
         y0: IMathNumber,
         R: IMathNumber,
         a: IMathNumber,
-        offset: IMathNumber = 0.mathNumber()
+        offset: IMathNumber
     ): IPoint2D {
         val resultX = x0 + R * (a + offset).toRadians().cos()
         val resultY = y0 + R * (a + offset).toRadians().sin()
