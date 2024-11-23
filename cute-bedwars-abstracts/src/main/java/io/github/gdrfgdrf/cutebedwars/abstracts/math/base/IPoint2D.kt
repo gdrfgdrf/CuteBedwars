5247e7431576aaf26517c7a3c774f5e2fb072fa1
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-11-23 19:41:30 +0800

    abstract point 2d

---
+++ cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/math/base/IPoint2D.kt
@@ -0,0 +1,23 @@
+package io.github.gdrfgdrf.cutebedwars.abstracts.math.base
+
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.IMathNumber
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.mathNumber
+import io.github.gdrfgdrf.multimodulemediator.Mediator
+import io.github.gdrfgdrf.multimodulemediator.annotation.Service
+import io.github.gdrfgdrf.multimodulemediator.bean.ArgumentSet
+
+@Service("point_2d", singleton = false)
+interface IPoint2D : IPoint {
+    val x: IMathNumber
+    val y: IMathNumber
+
+    companion object {
+        fun new(x: IMathNumber, y: IMathNumber): IPoint2D =
+            Mediator.get(IPoint2D::class.java, ArgumentSet(arrayOf(x, y)))!!
+
+        fun new(x: Number, y: Number): IPoint2D = new(
+            x.mathNumber(),
+            y.mathNumber()
+        )
+    }
+}
--- cute-bedwars-math/src/main/java/io/github/gdrfgdrf/cutebedwars/math/common/Point2D.kt
+++ cute-bedwars-math/src/main/java/io/github/gdrfgdrf/cutebedwars/math/common/Point2D.kt
@@ -2,12 +2,22 @@ package io.github.gdrfgdrf.cutebedwars.math.common
 
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.IMathNumber
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.base.IPoint
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.base.IPoint2D
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.base.IPoint3D
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.mathNumber
+import io.github.gdrfgdrf.multimodulemediator.annotation.ServiceImpl
+import io.github.gdrfgdrf.multimodulemediator.bean.ArgumentSet
 
+@ServiceImpl("point_2d", needArgument = true)
 class Point2D(
-    private val x: IMathNumber,
-    private val y: IMathNumber
-) : IPoint {
+    override val x: IMathNumber,
+    override val y: IMathNumber
+) : IPoint2D {
+    constructor(argumentSet: ArgumentSet): this(
+        argumentSet.args[0] as IMathNumber,
+        argumentSet.args[1] as IMathNumber
+    )
+
     override val step: Int = 2
     override val all: Array<IMathNumber> = arrayOf(x, y)
 
