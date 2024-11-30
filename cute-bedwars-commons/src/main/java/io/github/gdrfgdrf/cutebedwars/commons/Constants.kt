93fc4b94e3d6f7b75023b055b9cd04e1e794646f
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-11-30 18:15:49 +0800

    another config path in constants

--- cute-bedwars-commons/src/main/java/io/github/gdrfgdrf/cutebedwars/commons/Constants.kt
+++ cute-bedwars-commons/src/main/java/io/github/gdrfgdrf/cutebedwars/commons/Constants.kt
@@ -8,6 +8,8 @@ object Constants : IConstants {
     const val BASE_FOLDER = "cute-bedwars/"
     const val CONFIG_FILE_NAME = "config.json"
 
+    const val ANOTHER_CONFIG_PATH = BASE_FOLDER + "configs/"
+
     const val LANGUAGE_FOLDER = BASE_FOLDER + "languages"
     const val DEFAULT_DATABASE_FILE_NAME = BASE_FOLDER + "cute-bedwars-default-database-sqlite.db"
     const val DATABASE_IMPL_DESCRIPTION_FILE_NAME = "database-impl.json"
