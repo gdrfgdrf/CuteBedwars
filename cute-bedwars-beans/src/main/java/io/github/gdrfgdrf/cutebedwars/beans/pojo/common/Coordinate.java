667a7b607fcfe9e3b23f873a169b74dd87b96389
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-12-04 16:48:37 +0800

    parse function for Coordinate

--- cute-bedwars-beans/src/main/java/io/github/gdrfgdrf/cutebedwars/beans/pojo/common/Coordinate.java
+++ cute-bedwars-beans/src/main/java/io/github/gdrfgdrf/cutebedwars/beans/pojo/common/Coordinate.java
@@ -27,6 +27,22 @@ public class Coordinate {
         return "(%s, %s, %s)".formatted(x, y, z);
     }
 
+    public static Coordinate parse(String string) {
+        String[] split = string.split(" ");
+        if (split.length != 3) {
+            throw new IllegalArgumentException("the length of a string separated by a space must be 3");
+        }
+        double x = Double.parseDouble(split[0]);
+        double y = Double.parseDouble(split[1]);
+        double z = Double.parseDouble(split[2]);
+
+        Coordinate result = new Coordinate();
+        result.x = x;
+        result.y = y;
+        result.z = z;
+        return result;
+    }
+
     @SuppressWarnings("unchecked")
     @ConvertPropertyFunction
     public static <T> T convert(Class<?> targetType, Object obj) {
