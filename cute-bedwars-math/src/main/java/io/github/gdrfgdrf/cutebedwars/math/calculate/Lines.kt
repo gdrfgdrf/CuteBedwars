a071e6fb15ab83cc4ef87ecf401b226f971fd6ce
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-12-07 20:20:36 +0800

    findAPointCOnALineABInSpaceSuchThatTheDistanceOfBCIsAFixedValueD_TwoDimension method for lines

--- cute-bedwars-math/src/main/java/io/github/gdrfgdrf/cutebedwars/math/calculate/Lines.kt
+++ cute-bedwars-math/src/main/java/io/github/gdrfgdrf/cutebedwars/math/calculate/Lines.kt
@@ -4,6 +4,7 @@ import io.github.gdrfgdrf.cutebedwars.abstracts.math.IMathNumber
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.three.IPoint3D
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.calculate.ILines
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.common.minus
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.two.IPoint2D
 import io.github.gdrfgdrf.cutebedwars.math.enums.Dimensions
 import io.github.gdrfgdrf.cutebedwars.math.enums.Spaces
 import io.github.gdrfgdrf.cutebedwars.math.formula.distance.DistanceFormula
@@ -27,5 +28,20 @@ object Lines : ILines {
         val z = z1 + ((1 - (D / norm)) * (z2 - z1))
 
         return IPoint3D.new(x, y, z)
+    }
+
+    override fun findAPointCOnALineABInSpaceSuchThatTheDistanceOfBCIsAFixedValueD_TwoDimension(A: IPoint2D, B: IPoint2D, D: IMathNumber): IPoint2D {
+        val norm = DistanceFormula.calculate(Spaces.EUCLIDEAN, Dimensions.TWO, A, B)
+
+        val x1 = A.x
+        val y1 = A.y
+
+        val x2 = B.x
+        val y2 = B.y
+
+        val x = x1 + ((1 - (D / norm)) * (x2 - x1))
+        val y = y1 + ((1 - (D / norm)) * (y2 - y1))
+
+        return IPoint2D.new(x, y)
     }
 }
