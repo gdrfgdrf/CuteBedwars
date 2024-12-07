6eb526832bb2d1b30d7eb85f668a045c8f757527
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-12-07 20:29:10 +0800

    abstract line 2d

---
+++ cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/math/geometry/two/ILine2D.kt
@@ -0,0 +1,25 @@
+package io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.two
+
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.IMathNumber
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.base.ILine
+import io.github.gdrfgdrf.multimodulemediator.Mediator
+import io.github.gdrfgdrf.multimodulemediator.annotation.Service
+import io.github.gdrfgdrf.multimodulemediator.bean.ArgumentSet
+
+@Service("line_2d", singleton = false)
+interface ILine2D : ILine {
+    companion object {
+        fun new(start: IPoint2D, end: IPoint2D): ILine2D =
+            Mediator.get(ILine2D::class.java, ArgumentSet(arrayOf(start, end)))!!
+
+        fun new(
+            x1: IMathNumber,
+            y1: IMathNumber,
+            x2: IMathNumber,
+            y2: IMathNumber,
+        ): ILine2D = new(
+            IPoint2D.new(x1, y1),
+            IPoint2D.new(x2, y2)
+        )
+    }
+}
