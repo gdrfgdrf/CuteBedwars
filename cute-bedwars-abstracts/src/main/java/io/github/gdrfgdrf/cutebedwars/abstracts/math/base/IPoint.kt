68d3946e3326a7605d9a4891f0af9bccd0070aae
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-12-07 14:12:20 +0800

    check that a point is correct

--- cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/math/base/IPoint.kt
+++ cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/math/base/IPoint.kt
@@ -5,4 +5,13 @@ import io.github.gdrfgdrf.cutebedwars.abstracts.math.IMathNumber
 interface IPoint {
     val step: Int
     val numbers: Array<IMathNumber>
+
+    fun isValid(): Boolean {
+        numbers.forEach { number ->
+            if (number.toDouble().isNaN()) {
+                return false
+            }
+        }
+        return true
+    }
 }
