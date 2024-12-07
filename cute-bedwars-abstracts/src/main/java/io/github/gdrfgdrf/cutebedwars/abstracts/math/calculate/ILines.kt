dc88373b0ce957d5bd68870fa837482abbfc8180
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-12-07 20:28:57 +0800

    abstract findAPointCOnALineABInSpaceSuchThatTheDistanceOfBCIsAFixedValueD_TwoDimension

--- cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/math/calculate/ILines.kt
+++ cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/math/calculate/ILines.kt
@@ -2,6 +2,7 @@ package io.github.gdrfgdrf.cutebedwars.abstracts.math.calculate
 
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.IMathNumber
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.three.IPoint3D
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.two.IPoint2D
 import io.github.gdrfgdrf.multimodulemediator.Mediator
 import io.github.gdrfgdrf.multimodulemediator.annotation.KotlinSingleton
 import io.github.gdrfgdrf.multimodulemediator.annotation.Service
@@ -13,7 +14,12 @@ interface ILines {
      * 在三维空间中的一条直线 AB 上寻找一个点 C，使得 BC 的距离为一个定值 D
      * 返回值为点 C
      */
-    fun findAPointCOnALineABInSpaceSuchThatTheDistanceOfBCIsAFixedValueD(A: IPoint3D, B: IPoint3D, D: IMathNumber): IPoint3D
+    fun findAPointCOnALineABInSpaceSuchThatTheDistanceOfBCIsAFixedValueD_ThreeDimension(A: IPoint3D, B: IPoint3D, D: IMathNumber): IPoint3D
+    /**
+     * 在二维空间中的一条直线 AB 上寻找一个点 C，使得 BC 的距离为一个定值 D
+     * 返回值为点 C
+     */
+    fun findAPointCOnALineABInSpaceSuchThatTheDistanceOfBCIsAFixedValueD_TwoDimension(A: IPoint2D, B: IPoint2D, D: IMathNumber): IPoint2D
 
     companion object {
         fun instance(): ILines = Mediator.get(ILines::class.java)!!
