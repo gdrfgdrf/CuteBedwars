1eef25a64865d763ddd6cf9b8ae452b577e29e14
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-12-08 11:04:42 +0800

    add code comment

--- cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/math/geometry/calculate/IRotates.kt
+++ cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/math/geometry/calculate/IRotates.kt
@@ -14,6 +14,9 @@ interface IRotates {
      * origin: 被旋转的点
      * theta: 旋转角度 (角度制，运行时会转为弧度制)
      * 返回值为旋转后的二维点
+     *
+     * x' = cos(theta) * (x - rx) - sin(theta) * (y - ry) + rx
+     * y' = sin(theta) * (x - rx) + cos(theta) * (y - ry) + ry
      */
     fun rotatePoint2d(center: IPoint2D, origin: IPoint2D, theta: IMathNumber): IPoint2D
 }
