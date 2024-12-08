7595af7270c34011191767ac2c43d958ddf96cf6
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-12-08 12:08:16 +0800

    rotate point 3d

--- cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/math/geometry/calculate/IRotates.kt
+++ cute-bedwars-abstracts/src/main/java/io/github/gdrfgdrf/cutebedwars/abstracts/math/geometry/calculate/IRotates.kt
@@ -1,6 +1,9 @@
 package io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.calculate
 
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.IMathNumber
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.common.minus
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.three.IPoint3D
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.three.IVector3i
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.two.IPoint2D
 import io.github.gdrfgdrf.multimodulemediator.annotation.KotlinSingleton
 import io.github.gdrfgdrf.multimodulemediator.annotation.Service
@@ -19,4 +22,36 @@ interface IRotates {
      * y' = sin(theta) * (x - rx) + cos(theta) * (y - ry) + ry
      */
     fun rotatePoint2d(center: IPoint2D, origin: IPoint2D, theta: IMathNumber): IPoint2D
+    /**
+     * 将三维点绕 X 轴旋转,
+     * P: 原始点,
+     * a: 旋转角度 (角度制，运行时转为弧度制)
+     * n 的取值为 1, 0, 0
+     */
+    fun rotatePoint3dXAxis(P: IPoint3D, a: IMathNumber): IPoint3D
+    /**
+     * 将三维点绕 Y 轴旋转,
+     * P: 原始点,
+     * a: 旋转角度 (角度制，运行时转为弧度制)
+     * n 的取值为 0, 1, 0
+     */
+    fun rotatePoint3dYAxis(P: IPoint3D, a: IMathNumber): IPoint3D
+    /**
+     * 将三维点绕 Z 轴旋转,
+     * P: 原始点,
+     * a: 旋转角度 (角度制，运行时转为弧度制)
+     * n 的取值为 0, 0, 1
+     */
+    fun rotatePoint3dZAxis(P: IPoint3D, a: IMathNumber): IPoint3D
+    /**
+     * 基于 https://blog.csdn.net/maple_2014/article/details/104443928，
+     * 将某点绕某个轴旋转指定角度
+     *
+     * Q 的取值为 0, 0, 0
+     * P: 原始点,
+     * n: 旋转轴的单位方向向量
+     * a: 旋转角度 (角度制，运行时转为弧度制)
+     * 返回旋转后的点
+     */
+    fun rotatePoint3d(P: IPoint3D, n: IVector3i, a: IMathNumber): IPoint3D
 }
--- cute-bedwars-math/cute-bedwars-geometry/src/main/java/io/github/gdrfgdrf/cutebedwars/math/geometry/calculate/Rotates.kt
+++ cute-bedwars-math/cute-bedwars-geometry/src/main/java/io/github/gdrfgdrf/cutebedwars/math/geometry/calculate/Rotates.kt
@@ -1,7 +1,10 @@
 package io.github.gdrfgdrf.cutebedwars.math.geometry.calculate
 
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.IMathNumber
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.common.minus
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.calculate.IRotates
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.three.IPoint3D
+import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.three.IVector3i
 import io.github.gdrfgdrf.cutebedwars.abstracts.math.geometry.two.IPoint2D
 import io.github.gdrfgdrf.cutebedwars.math.geometry.two.Point2D
 import io.github.gdrfgdrf.multimodulemediator.annotation.ServiceImpl
@@ -21,5 +24,70 @@ object Rotates : IRotates {
         val resultY = sinTheta * (x - rx) + cosTheta * (y - ry) + ry
 
         return Point2D.of(resultX, resultY)
+    }
+
+    override fun rotatePoint3dXAxis(P: IPoint3D, a: IMathNumber): IPoint3D {
+        val n = IVector3i.new(1, 0, 0)
+        return rotatePoint3d(P, n, a)
+    }
+
+    override fun rotatePoint3dYAxis(P: IPoint3D, a: IMathNumber): IPoint3D {
+        val n = IVector3i.new(0, 1, 0)
+        return rotatePoint3d(P, n, a)
+    }
+
+    override fun rotatePoint3dZAxis(P: IPoint3D, a: IMathNumber): IPoint3D {
+        val n = IVector3i.new(0, 0, 1)
+        return rotatePoint3d(P, n, a)
+    }
+
+    override fun rotatePoint3d(
+        P: IPoint3D,
+        n: IVector3i,
+        a: IMathNumber
+    ): IPoint3D {
+        val theta = a.radians()
+
+        val px = P.x
+        val py = P.y
+        val pz = P.z
+
+        val nx = n.x
+        val ny = n.y
+        val nz = n.z
+
+        val t0 = nx * px + ny * py + nz * pz
+
+        val xc = nx * t0
+        val yc = ny * t0
+        val zc = nz * t0
+
+        val r = ((px - xc) * (px - xc) + ((py - yc) * (py - yc)) + ((pz - zc) * (pz - zc))).sqrt()
+
+        val opX = (px - xc) / r
+        val opY = (py - yc) / r
+        val opZ = (pz - zc) / r
+
+        val yPrimeX = nx * opY - ny * opZ
+        val yPrimeY = ny * opZ - nz * opX
+
+        val cosTheta = theta.cos()
+        val sinTheta = theta.sin()
+
+        val R11 = nx * nx + (ny * ny + nz * nz) * cosTheta
+        val R12 = nx * ny * (1 - cosTheta) - nz * sinTheta
+        val R13 = nx * nz * (1 - cosTheta) + ny * sinTheta
+        val R21 = ny * nx * (1 - cosTheta) + nz * sinTheta
+        val R22 = ny * ny + (nx * nx + nz * nz) * cosTheta
+        val R23 = ny * nz * (1 - cosTheta) - nx * sinTheta
+        val R31 = nz * nx * (1 - cosTheta) - ny * sinTheta
+        val R32 = nz * ny * (1 - cosTheta) + nx * sinTheta
+        val R33 = nz * nz + (nx * nx + ny * ny) * cosTheta
+
+        val pxPrime = R11 * opX + R12 * yPrimeX + R13 * yPrimeY + xc
+        val pyPrime = R21 * opX + R22 * yPrimeX + R23 * yPrimeY + yc
+        val pzPrime = R31 * opX + R32 * yPrimeX + R33 * yPrimeY + zc
+
+        return IPoint3D.new(pxPrime, pyPrime, pzPrime)
     }
 }
