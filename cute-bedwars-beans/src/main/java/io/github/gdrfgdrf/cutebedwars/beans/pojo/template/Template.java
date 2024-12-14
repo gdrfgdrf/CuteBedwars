90b927c617bb07ff9cb98927486733d16126f1d3
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-12-14 22:31:11 +0800

    remove convertible method for the template

--- cute-bedwars-beans/src/main/java/io/github/gdrfgdrf/cutebedwars/beans/pojo/template/Template.java
+++ cute-bedwars-beans/src/main/java/io/github/gdrfgdrf/cutebedwars/beans/pojo/template/Template.java
@@ -1,7 +1,6 @@
 package io.github.gdrfgdrf.cutebedwars.beans.pojo.template;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import io.github.gdrfgdrf.cutebedwars.beans.annotation.ConvertPropertyFunction;
 import io.github.gdrfgdrf.cutebedwars.beans.annotation.UndefinableForPropertyChange;
 import io.github.gdrfgdrf.cutebedwars.beans.pojo.game.Game;
 import lombok.Data;
@@ -17,13 +16,4 @@ public class Template {
     private String name;
     @UndefinableForPropertyChange
     private Game game;
-
-    @SuppressWarnings("unchecked")
-    @ConvertPropertyFunction
-    public static <T> T convert(Class<?> targetType, Object obj) {
-        if (targetType == String.class) {
-            return (T) obj.toString();
-        }
-        return null;
-    }
 }
